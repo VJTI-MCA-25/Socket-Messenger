05f557f0c9461fcf213f845c661ecab0b538611d
Aashay-Thakur <thakur.aashay@gmail.com>
2024-03-30 12:44:49 +0530

    Refactor friend deletion and group creation logic

--- backend/routes/friendsRoutes.js
+++ backend/routes/friendsRoutes.js
@@ -20,36 +20,48 @@ friends.delete("/:uid", async (req, res) => {
 
 	try {
 		let userData = (await usersRef.doc(user.uid).get()).data();
-		let userFriendsList = userData.friends.map((friend) => friend.uid);
+		let userFriendsList = Object.keys(userData.friends);
 
-		let friendRef = usersRef.doc(uid);
-		let friendData = (await friendRef.get()).data();
+		let friendDoc = await usersRef.doc(uid).get();
+		let friendData = friendDoc.data();
+		let friendFriendsList = Object.keys(friendData.friends);
 
 		if (user.uid === uid) throw CannotRemoveSelfError;
-		if (!(await friendRef.get()).exists) throw UserNotFoundError;
+		if (!friendDoc.exists) throw UserNotFoundError;
 		if (!userFriendsList.includes(uid)) throw ProvidedUidNotAFriendError;
 
-		userFriendsList = userData.friends.filter((friend) => friend.uid !== uid);
-		let friendFriendsList = friendData.friends.filter((friend) => friend.uid !== user.uid);
+		let newFriendsList_user = userFriendsList.reduce((acc, friendUid) => {
+			if (friendUid !== uid) {
+				acc[friendUid] = userData.friends[friendUid];
+			}
+			return acc;
+		}, {});
+
+		let newFriendsList_friend = friendFriendsList.reduce((acc, friendUid) => {
+			if (friendUid !== user.uid) {
+				acc[friendUid] = friendData.friends[friendUid];
+			}
+			return acc;
+		}, {});
 
 		await Promise.all([
 			usersRef.doc(user.uid).update({
-				friends: userFriendsList,
+				friends: newFriendsList_user,
 			}),
 			usersRef.doc(uid).update({
-				friends: friendFriendsList,
+				friends: newFriendsList_friend,
 			}),
 		]);
 
 		res.status(200).send("auth/friend-removed");
 	} catch (error) {
-		errorHandler(res, error);
+		return errorHandler(res, error);
 	}
 });
 
 friends.post("/create-group", async (req, res) => {
 	const user = req.user;
-	var list = req.body.list;
+	let list = req.body.list;
 
 	if (!list) throw MissingParametersError;
 	if (typeof list === "string") list = [list];
@@ -57,11 +69,11 @@ friends.post("/create-group", async (req, res) => {
 	if (list.includes(user.uid)) throw CannotCreateGroupWithSelfError;
 
 	try {
-		const friends = (await usersRef.doc(user.uid).get()).data()?.friends || {};
-		const friendsList = Object.keys(friends);
+		const friends_user = (await usersRef.doc(user.uid).get()).data()?.friends || {};
+		const friendsList_user = Object.keys(friends_user);
 		const sortedUserIds = [...list, user.uid].sort().join(",");
 
-		if (!list.every((uid) => friendsList.includes(uid))) throw ProvidedUidNotAFriendError;
+		if (!list.every((uid) => friendsList_user.includes(uid))) throw ProvidedUidNotAFriendError;
 		if (!(await groupsRef.where("sortedUserIds", "==", sortedUserIds).get()).empty) throw GroupAlreadyExists; // Probably should remove this limitation
 
 		let data = {
@@ -77,23 +89,23 @@ friends.post("/create-group", async (req, res) => {
 
 		if (list.length === 1) {
 			let friendId = list[0];
-			let userFriends = (await usersRef.doc(user.uid).get()).data()?.friends || {};
+			let friends_friend = (await usersRef.doc(friendId).get()).data()?.friends || {};
 
 			await Promise.all([
 				usersRef.doc(user.uid).update({
 					friends: {
-						...userFriends,
+						...friends_user,
 						[friendId]: {
-							...userFriends[friendId],
+							...friends_friend[friendId],
 							dm: groupId,
 						},
 					},
 				}),
 				usersRef.doc(friendId).update({
 					friends: {
-						...friends,
+						...friends_friend,
 						[user.uid]: {
-							...friends[user.uid],
+							...friends_friend[user.uid],
 							dm: groupId,
 						},
 					},
